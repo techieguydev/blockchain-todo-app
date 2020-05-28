d4ea793b4b7afc09513b77d6ff03afc4e3510988
Qadir Hussain <qadir.hussain99@gmail.com>
2020-05-29 03:06:49 +0400

    Create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,30 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // state variable
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    constructor() public {
+        createTask("Checkout techieguy.dev");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false)
+    }
+
 }
